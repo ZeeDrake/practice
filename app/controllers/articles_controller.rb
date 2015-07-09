3ae5ec84069e214a6eec33deadfd20652a481eef
ZeeDrake <ZeeDrake3000@gmail.com>
2015-07-08 19:33:50 -0500

    Added routes and text to homepage/routes

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+	def index
+		@potty_time = "Potty time takes THREE Qualities..."
+		@eating_time = "COLOR IS BEST"
+	end
+
+	def edit
+		
+	end
+
+	def new
+		
+	end
+
+	def show
+		
+	end
+
+	def create
+		
+	end
+
+	def update
+		
+	end
+
+	def destory
+		
+	end
+end
