6c2f4ec1d39dce2b0d6af59c8edb21f4f7eb1010
ZeeDrake <ZeeDrake3000@gmail.com>
2015-07-14 01:41:24 -0500

    Created new model & played with record editing functions in rails console

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150712043324) do
+ActiveRecord::Schema.define(version: 20150714043622) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "tile"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20150712043324) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "monsters", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "hitpoints"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
